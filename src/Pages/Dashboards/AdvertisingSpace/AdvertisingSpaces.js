f9e730f58467c52d69442285368c7cfa354c3571
Rahul mallik <rmallik191242@bscse.uiu.ac.bd>
2025-05-13 11:35:36 +0600

    add AdvertisingSpace component with pagination and common bar; create AdvertisingSpaces data file

---
+++ src/Pages/Dashboards/AdvertisingSpace/AdvertisingSpaces.js
@@ -0,0 +1,19 @@
+const AdvertisingSpaces = [
+  {
+    name: "Olive Oil",
+    image: "https://skyandsol.co/cdn/shop/articles/olives-and-olive-oil-2023-03-17-17-20-24-utc_1_1_1_f69d3870-4ef2-4cea-bf14-f16142d82a38.jpg?v=1740573025",
+    
+  },
+  {
+    name: "Coconut Oil",
+    image: "https://skyandsol.co/cdn/shop/articles/olives-and-olive-oil-2023-03-17-17-20-24-utc_1_1_1_f69d3870-4ef2-4cea-bf14-f16142d82a38.jpg?v=1740573025",
+    
+  },
+  {
+    name: "Argan Oil",
+    image: "https://skyandsol.co/cdn/shop/articles/olives-and-olive-oil-2023-03-17-17-20-24-utc_1_1_1_f69d3870-4ef2-4cea-bf14-f16142d82a38.jpg?v=1740573025",
+    
+  },
+]
+
+export default AdvertisingSpaces;
