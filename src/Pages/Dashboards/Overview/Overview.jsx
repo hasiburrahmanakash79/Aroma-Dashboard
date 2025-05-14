f332398d7df9ece898b0c23079e9fadd2c056669
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-05-14 11:10:48 +0600

    create authentiction pages

--- src/Pages/Dashboards/Overview/Overview.jsx
+++ src/Pages/Dashboards/Overview/Overview.jsx
@@ -6,13 +6,13 @@ const Overview = () => {
     <div className="p-4">
       <h1 className="text-2xl font-semibold mb-5">Overview</h1>
       <div className="flex justify-center items-center gap-10 py-10">
-        <div className="border border-base-300 py-7 px-20 rounded-xl text-center ">
+        <div className="border border-gray-200 py-7 px-20 rounded-xl text-center shadow-lg ">
           <p>Total Subscribe</p>
           <h1 className="text-5xl font-semibold pt-2">$1200</h1>
         </div>
-        <div className="border border-base-300 py-7 px-20 rounded-xl text-center ">
+        <div className="border border-gray-200 py-7 px-20 rounded-xl text-center shadow-lg ">
           <p>Total Earnings</p>
-          <h1 className="text-5xl font-semibold pt-2">$1200</h1>
+          <h1 className="text-5xl font-semibold pt-2">$9000</h1>
         </div>
       </div>
       <EarningProgressBar />
