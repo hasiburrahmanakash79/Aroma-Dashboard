a753de28910f391b15bc24b76123ad37adee44cd
hasiburrahmanakash79 <hasiburrahmanakash79@gmail.com>
2025-05-20 13:13:30 +0600

    change the advertising space

--- src/Pages/Dashboards/AdvertisingSpace/AddAdvertisingSpace.jsx
+++ src/Pages/Dashboards/AdvertisingSpace/AddAdvertisingSpace.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import toast from "react-hot-toast";
-import { IoArrowBackOutline } from "react-icons/io5";
+import { IoArrowBackOutline, IoCloudUploadOutline } from "react-icons/io5";
 import { Link } from "react-router-dom";
 
 const AddAdvertisingSpace = () => {
@@ -74,48 +74,30 @@ const AddAdvertisingSpace = () => {
       >
         {/* Image Upload */}
         <div className="mb-6">
-          <div className="flex flex-col items-center justify-center rounded-lg w-50 h-64 border border-gray-300 border-gray-200rounded-lg bg-gray-100 cursor-pointer hover:bg-gray-200 mb-2">
+          <label
+            htmlFor="image-upload"
+            className="flex flex-col items-center justify-center w-50 h-64 border border-gray-200 rounded-lg bg-gray-50 cursor-pointer hover:bg-gray-100 mb-2"
+          >
             {previewImage ? (
               <img
                 src={previewImage}
-                alt="Course Preview"
+                alt="Preview"
                 className="w-full h-full object-cover rounded-lg"
               />
             ) : (
-              <div className="flex flex-col items-center justify-center p-6">
-                <svg
-                  className="w-10 h-10 mb-3 text-gray-400"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    strokeWidth="2"
-                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-                  />
-                </svg>
-                <p className="mb-2 text-sm text-gray-500">
-                  <input
-                    type="file"
-                    className="hidden"
-                    accept="image/*"
-                    id="image-upload"
-                    onChange={handleImageUpload}
-                  />
-                  {/* <span className="font-semibold">Upload Image</span> */}
-                  <label
-                    htmlFor="image-upload"
-                    className="block text-sm font-medium text-center cursor-pointer"
-                  >
-                    Upload Image
-                  </label>
-                </p>
+              <div className="flex flex-col items-center justify-center p-6 text-gray-600">
+                <IoCloudUploadOutline className="text-4xl" />
+                <p className="my-2 text-sm font-semibold">Upload Image</p>
               </div>
             )}
-          </div>
+            <input
+              type="file"
+              className="hidden"
+              accept="image/*"
+              id="image-upload"
+              onChange={handleImageUpload}
+            />
+          </label>
         </div>
 
         {/* Course Title and Link */}
